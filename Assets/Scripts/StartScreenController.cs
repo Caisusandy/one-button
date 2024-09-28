deb0656011ea110ed5107fc707d938a2fb68cd40
CrystalGmr <arfrenchhorn@gmail.com>
2024-09-28 10:41:29 -0500

    Created Start Screen
    I created the UI for the start screen and created a script to transition from the start screen to the main game.
    
    Note: The game still needs a title and I'm not sure how to get the game to start on the start screen by default.

---
+++ Assets/Scripts/StartScreenController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OneButton
+{
+    public class StartScreenController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (!GameManager.instance.isEndGame && Input.GetKeyUp("space"))
+            {
+                StartGame();
+            }
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
